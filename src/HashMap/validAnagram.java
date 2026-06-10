258cac35f4a921a9e56dfd833d5faacfa2bced39
ashutosh8555 <thakurashutoshsingh09@gmail.com>
2026-06-11 00:47:40 +0530

    10 june 2026

---
+++ src/HashMap/validAnagram.java
@@ -0,0 +1,38 @@
+package HashMap;
+import java.util.*;
+
+
+public class validAnagram {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String");
+        String s=sc.nextLine();
+        String t=sc.nextLine();
+
+        System.out.println(isAnagram(s,t));
+    }
+
+    static HashMap<Character, Integer> makeFreqMap(String str){
+        HashMap<Character, Integer> mp= new HashMap<>();
+        for(int i=0; i<str.length(); i++){
+            Character ch=str.charAt(i);
+            if(!mp.containsKey(ch)){
+                mp.put(ch,1);
+            }
+            else{
+                int currfreq = mp.get(ch);
+                mp.put(ch, currfreq+1);
+            }
+        }
+        return mp;
+    }
+
+    public static boolean isAnagram(String s, String t){
+        if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character,Integer> mp1=makeFreqMap(s);
+        HashMap<Character,Integer> mp2=makeFreqMap(t);
+        return mp1.equals(mp2);
+    }
+}
