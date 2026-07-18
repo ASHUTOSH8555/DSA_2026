0011915cb01e64d831006e968da098c6f6d7dc77
ashutosh8555 <thakurashutoshsingh09@gmail.com>
2026-07-18 23:46:41 +0530

    18 july 2026

---
+++ src/Recursion/SubSequence.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class SubSequence {
+    public static void main(String[] args)
+    {
+        Subseq("","abcs");
+
+    }
+    public static void Subseq(String p , String up)
+    {
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        Subseq(p+ ch , up.substring(1));
+        Subseq(p, up.substring(1));
+    }
+}
